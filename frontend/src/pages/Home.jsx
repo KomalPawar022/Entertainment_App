8a552fb73de86dd6413b35ce52a5bcf03b5b4ff3
Gauri Pawar <gauripawar022@gmail.com>
2024-04-17 19:34:58 +0000

    home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,61 @@
 import { Box } from "@mui/material";
+import { MdMovie } from "react-icons/md";
+import { red } from "@mui/material/colors";
+import { PiSquaresFourFill } from "react-icons/pi";
+import { MdLocalMovies } from "react-icons/md";
+import { PiTelevisionBold } from "react-icons/pi";
+import { FaBookmark } from "react-icons/fa6";
+import { Avatar } from "@mui/material";
 const Home = () => {
-  return <Box>Home</Box>;
+  return (
+    <Box
+      sx={{
+        diaplay: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 3,
+          backgroundColor: "#191B34",
+          borderRadius: 3,
+          width: "70px",
+          height: "80vh",
+          justifyContent: "top",
+          alignItems: "center",
+          marginLeft: "25px",
+        }}
+      >
+        <MdMovie
+          style={{
+            color: red[400],
+            width: "30px",
+            height: "30px",
+            marginBottom: "12px",
+            marginTop: "5px",
+          }}
+        />
+        <PiSquaresFourFill
+          style={{ color: "#444657", width: "30px", height: "30px" }}
+        />
+        <MdLocalMovies
+          style={{ color: "#444657", width: "30px", height: "30px" }}
+        />
+        <PiTelevisionBold
+          style={{ color: "#444657", width: "30px", height: "30px" }}
+        />
+        <FaBookmark
+          style={{ color: "#444657", width: "30px", height: "30px" }}
+        />
+        <Avatar sx={{ justifySelf: "bottom" }} />
+      </Box>
+      <Box></Box>
+    </Box>
+  );
 };
 export default Home;
